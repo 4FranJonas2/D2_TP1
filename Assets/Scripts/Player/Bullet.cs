ede2eb53ad4d12b6f74fb7c32d45b50f58e7cdc1
4FranJonas2 <franjonas98@hotmail.com>
2026-04-20 12:40:39 -0300

    Fixed bullet despawn function

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -1,23 +1,28 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class Bullet : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private GameObject explosionEffect;
+    [SerializeField] private float timeAlive = 5.0f;
 
     private void Awake  ()
     {
         rb = GetComponent<Rigidbody>();
     }
+
     public void Logic(float speed)
     {
         rb.AddForce(transform.forward * speed);
+        Destroy(gameObject, timeAlive);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        var bullet = Instantiate(explosionEffect, transform.position, Quaternion.identity);
-       // Destroy(bullet.gameObject, 2f);
+        Instantiate(explosionEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
+        Destroy(other.gameObject);
     }
 }
